de80e6fabd5b9d24eede0aef13a8bbce1080d894
vlad-seredenko <78535638+vlad-seredenko@users.noreply.github.com>
2021-03-03 20:22:21 +0200

    Add files via upload

---
+++ Lab-2a_individual_task.lib/Tablet.lib.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab2alibs
+
+{
+    public class Tablet
+    {
+        public string Brand;
+        public string Model;
+        public string Color;
+        public int Memory;
+        public double Year;
+        public double SizeDisplay;
+        public bool Bluetooth;
+        public bool NFC;
+        public int Number;
+        public double TotalPrice;
+
+
+        public double GetYearIncomePerInhabitant()
+        {
+            return TotalPrice / Number;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
